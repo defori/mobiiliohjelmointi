2cf678a3dfa136d0ff9347cfc95c3b58a5c57b59
defori <jami.linnanen@outlook.com>
2022-09-07 23:29:50 +0300

    tehtävä valmis

--- ostoslista/App.js
+++ ostoslista/App.js
@@ -1,73 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Button, Alert, TextInput, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, View, Text, Button, Alert, TextInput, Image, SafeAreaView, Flatlist } from 'react-native';
 
-const App = () => {
-  
-  const [text, onChangeText] = React.useState();
-  const [number, onChangeNumber] = React.useState(null);
+export default function App(){
+  const [text, setText] = useState('');
+  const [data, setData] = UseState([]);
+
+  const buttonPressed = () => {
+    const newData = [...data,text];
+    setData(newData);
+    setText('');
+    console.log(newData);
+  }
+
 
   
 
   return (
-    <View>
-      <Text style={styles.title}>
-        
-      </Text>
-      <View style={styles.fixToText}>
-        <Button
-          title="+"
-          onPress={() => Alert.alert('Left button pressed')}
+    <View style={styles.container}>
+      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text}/>
+      <Button titel='Add to list' onPress={buttonPressed} />
+      <Flatlist
+        data={data}
+        renderItem={({item}) => <Text>{item}</Text>}
         />
-        <Button
-          title="-"
-          onPress={() => Alert.alert('Right button pressed')}
-        />
+        <StatusBar style="auto" />
       </View>
-    
-    
-    <SafeAreaView>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-        keyboardType="numeric"
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder=""
-        keyboardType="numeric"
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-        keyboardType="numeric"
-      />
-    </SafeAreaView>
-    </View>
   );
-};
+}
 
 
 const styles = StyleSheet.create({
-  input: {
+   container: {
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  screenContainer: {
-    flex: 1,
-    justifyContent: "center",
-    padding: 16
-  },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+ 
+  input: {
+    borderColor:'black',
+    borderWidth: 1,
+    width: '50%',
+    marginTop: 12
   },
 });
-
-export default App;
